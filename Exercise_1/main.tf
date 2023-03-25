3dfb267731af4a44a2f26a3304b33637366b26e1
LamDT <dinhtienlam.spkt@gmail.com>
2023-03-25 23:26:21 +0700

    ex1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,27 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "ec2_T2" {
+  count         = var.T2_instances
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0381beeb41a93ecba"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "ec2_M4" {
+  count         = var.M4_instances
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "m4.large"
+  subnet_id     = "subnet-0381beeb41a93ecba"
+  tags = {
+    Name = "Udacity M4"
+  }
+}
---
+++ Exercise_1/variables.tf
@@ -0,0 +1,9 @@
+variable "T2_Instances" {
+  description = "ec2 T2 instances"
+  default = 4
+}
+
+variable "M4_Instances" {
+  description = "ec2 M4 instances"
+  default = 2
+}
