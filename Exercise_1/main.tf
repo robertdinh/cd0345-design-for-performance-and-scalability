4ec42d0c4c2961bca507fc8914692c42b48644be
LamDT <dinhtienlam.spkt@gmail.com>
2023-03-26 23:41:44 +0700

    ex2

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -6,10 +6,10 @@ provider "aws" {
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "ec2_T2" {
-  count         = var.T2_instances
+  count         = var.T2_Instances
   ami           = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0381beeb41a93ecba"
+  subnet_id     = "subnet-09d5d6300757d2b1f"
   tags = {
     Name = "Udacity T2"
   }
@@ -17,10 +17,10 @@ resource "aws_instance" "ec2_T2" {
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 resource "aws_instance" "ec2_M4" {
-  count         = var.M4_instances
+  count         = var.M4_Instances
   ami           = "ami-0022f774911c1d690"
   instance_type = "m4.large"
-  subnet_id     = "subnet-0381beeb41a93ecba"
+  subnet_id     = "subnet-09d5d6300757d2b1f"
   tags = {
     Name = "Udacity M4"
   }
--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,5 @@
 # TODO: Define the output variable for the lambda function.
+output "arn" {
+  description = "Lambda Function Arn"
+  value       = aws_lambda_function.greet_lambda.arn
+}
